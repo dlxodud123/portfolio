68f3766ce2f3a274e19732cb7460aecbbaeb3902
junhyeong9812 <pickjog@gmail.com>
2024-08-03 23:02:30 +0900

    글자 수정

--- src/main/js/end.js
+++ src/main/js/end.js
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 import './../css/end.css';
 
 
@@ -53,19 +53,39 @@ import './../css/end.css';
 //   export default End;
 
 const End = forwardRef((props, ref) => {
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setZoomLevel(window.devicePixelRatio);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // 초기 실행
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-      <div ref={ref} className="end-container">
-          <img 
+      <div ref={ref} className="end-container"  style={{ 
+        backgroundImage: `url(${process.env.PUBLIC_URL}/img/background/background.png)` ,
+        // backgroundSize: "cover" /* 컨테이너를 덮도록 설정 */,
+        backgroundSize: zoomLevel <0.75 ? "contain" : "cover", /* 확대 수준에 따라 조정 */
+        backgroundPosition: "center" /* 중앙에 배치 */
+      }}>
+          {/* <img 
               className="end-background" 
               src={`${process.env.PUBLIC_URL}/img/background/background.png`} 
               alt='background' 
-          />
-          <p className="end-main-text">
-              감사합니다!
-          </p>
-          <p className="end-sub-text">
-              Thank You
-          </p>
+          /> */}
+           <p className="end-main-text" >
+        감사합니다!
+      </p>
+      <p className="end-sub-text" 
+      >
+        Thank You
+      </p>
       </div>
   );
 });
