de20a24fe9927328e644206c4713ee4f61d8737e
김재환 <jawhan98@naver.com>
2024-08-05 13:45:42 +0900

    최종 수정

--- src/main/js/project.js
+++ src/main/js/project.js
@@ -60,7 +60,7 @@ const Project = forwardRef((props, ref) => {
                     <div className="project-card-desc">
                         KREAM 웹 사이트 클론 프로젝트<br />
                         <label>2024.06.02 ~ 2024.08.02</label>
-                        <p>KREAM 웹 사이트 클론 프로젝트를 진행하였습니다.<br/></p>
+                        <p>KREAM 웹 사이트 클론 프로젝트를 진행하였습니다.(React)<br/></p>
                         <p>크림 웹 사이트 : </p>
                         <a className='address' href='https://kream.co.kr/' >https://kream.co.kr/</a>
                         <br/>
@@ -72,12 +72,28 @@ const Project = forwardRef((props, ref) => {
                 </div>
                 <div className="project-card">
                     <div className="project-card-img">
-                        <img src={`${process.env.PUBLIC_URL}/img/project/project1.png`} alt='project1' />
+                        <img src={`${process.env.PUBLIC_URL}/img/project/project2.png`} alt='project1' />
                     </div>
                     <div className="project-card-desc">
                        협업 툴 프로젝트(Node.js)<br />
-                        <label>2024.01.31 ~ 2024.08.02</label>
-                        <p>sdasdasd</p>
+                        <label>2024.04.11 ~ 2024.04.25</label>
+                        <p>협업 툴 프로젝트 진행하였습니다.(node.js)</p>
+                        <p>깃허브 소스 코드 : </p>
+                        <a className='address' href='https://github.com/KimJaeHwan98/ForWebPrjBs' >https://github.com/KimJaeHwan98/ForWebPrjBs</a>
+                        <p>ppt : </p>
+                        <a className='address' href='https://docs.google.com/presentation/d/1FhtH7zdxHCUmf0rAK6oeOYmYHFsV6ROCL8g23di3FOE/edit#slide=id.g2ef1808f267_1_174' >ppt url</a>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-img">
+                        <img src={`${process.env.PUBLIC_URL}/img/project/project3.png`} alt='project1' />
+                    </div>
+                    <div className="project-card-desc">
+                        포트폴리오 프로젝트<br />
+                        <label>2024.07.25 ~ 2024.07.30</label>
+                        <p>입사지원 포트폴리오 제작 프로젝트 진행하였습니다.</p>
+                        <p>깃허브 소스 코드 : </p>
+                        <a className='address' href='https://github.com/KimJaeHwan98/portfolio' >https://github.com/KimJaeHwan98/portfolio</a>
                     </div>
                 </div>
             </div>
