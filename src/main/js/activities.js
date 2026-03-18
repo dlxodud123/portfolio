f83a1f69c7d376d969cbc89ce518889a58854dc6
LeeTaeYoung <xodud5080@naver.com>
2026-03-18 10:18:15 +0900

    modify portfolio

--- src/main/js/activities.js
+++ src/main/js/activities.js
@@ -17,7 +17,7 @@ const Activities = forwardRef((props, ref) => {
                         KG IT뱅크 부트 캠프 수료<br /><label>2024.01.31 ~ 2024.08.02</label>
                     </div>
                 </div>
-                <div className="activities-card" onClick={() => { window.location.href = 'https://codingapple.com/course/typescript-crash-course/' }}>
+                <div className="activities-card" onClick={() => { window.location.href = 'https://www.inflearn.com/search?s=%EA%B9%80%EC%98%81%ED%95%9C/' }}>
                     <div className="activities-img">
                         <img src={`${process.env.PUBLIC_URL}/img/activities/인프런.jpeg`} alt='coding apple typescript' />
                     </div>
@@ -25,7 +25,7 @@ const Activities = forwardRef((props, ref) => {
                         인프런 Spring DB, Spring Boot, JPA, Spring Data JPA, Querydsl 강의 수료
                     </div>
                 </div>
-                <div className="activities-card" onClick={() => { window.location.href = 'https://codingapple.com/course/typescript-crash-course/' }}>
+                <div className="activities-card" onClick={() => { window.location.href = 'https://codingapple.com/course/react-basic/' }}>
                     <div className="activities-img">
                         <img src={`${process.env.PUBLIC_URL}/img/activities/코딩애플.png`} alt='coding apple typescript' />
                     </div>
